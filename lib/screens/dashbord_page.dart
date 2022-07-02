3f15bfa602070fa1f29bf1239b3683f42ce89bf3
mohamed-achich <mohamed.achich27@gmail.com>
2022-07-02 21:00:46 +0100

    Update dashbord_page.dart

--- lib/screens/dashbord_page.dart
+++ lib/screens/dashbord_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:bee/constants/colors.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -13,7 +14,7 @@ class Dashboard extends StatefulWidget {
 //te5edh objet profile mel base
 class _MyDashboard extends State<Dashboard> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  int? temperature;
+  double? temperature;
   int? humidity;
   String? sound;
   StreamSubscription? _tempStream;
@@ -22,33 +23,35 @@ class _MyDashboard extends State<Dashboard> {
 
   @override
   void initState() {
-    super.initState();
     _activateListeners();
+    super.initState();
   }
 
   void _activateListeners() {
-    var value;
+    var tempValue;
+    var humValue;
+    var soundValue;
+    var weightValue;
     DatabaseReference tempRef = FirebaseDatabase.instance.ref("temperature");
     DatabaseReference humRef = FirebaseDatabase.instance.ref("humidity");
     DatabaseReference soundRef = FirebaseDatabase.instance.ref("sound");
-
     _tempStream = tempRef.onValue.listen((event) {
-      value = event.snapshot.value;
+      tempValue = event.snapshot.value;
       print(event.snapshot.value);
       setState(() {
-        temperature = value;
+        temperature = tempValue;
       });
     });
     _humStream = humRef.onValue.listen((event) {
-      value = event.snapshot.value;
+      humValue = event.snapshot.value;
       setState(() {
-        humidity = value;
+        humidity = humValue;
       });
     });
     _soundStream = soundRef.onValue.listen((event) {
-      value = event.snapshot.value;
+      soundValue = event.snapshot.value;
       setState(() {
-        sound = value;
+        sound = soundValue;
       });
     });
   }
@@ -57,134 +60,136 @@ class _MyDashboard extends State<Dashboard> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return SafeArea(
-      key: _scaffoldKey,
-      child: Scaffold(
-          body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [beeUp, beeUp])),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: 400,
-                  child: Image.asset(
-                    "assets/beeware.png",
-                    width: size.height * 0.18,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Column(
-                  children: [
-                    SizedBox(height: 70),
-                    Container(
-                      width: 400,
-                      height: 650,
-                      child: Image.asset(
-                        "assets/cells.png",
-                        fit: BoxFit.cover,
-                        height: size.height * 0.8,
+        key: _scaffoldKey,
+        child: Scaffold(
+          body: ListView(children: [
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [beeUp, beeUp])),
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: 400,
+                        child: Image.asset(
+                          "assets/beeware.png",
+                          width: size.height * 0.18,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.fromLTRB(100, 315, 5, 0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
+                      Column(
+                        children: [
+                          SizedBox(height: 70),
+                          Container(
+                            width: 400,
+                            height: 650,
+                            child: Image.asset(
+                              "assets/cells.png",
+                              fit: BoxFit.cover,
+                              height: size.height * 0.8,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Column(
                             children: [
-                              Text(
-                                "$temperature",
-                                style: TextStyle(
-                                  color: beeBrown,
-                                  fontFamily: 'Gilroy',
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 45,
+                              Container(
+                                margin: EdgeInsets.fromLTRB(100, 315, 5, 0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "$temperature",
+                                      style: TextStyle(
+                                        color: beeBrown,
+                                        fontFamily: 'Gilroy',
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 45,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 28),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(5, 0, 65, 0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text(
-                                "$humidity %",
-                                style: TextStyle(
-                                  color: beeBrown,
-                                  fontFamily: 'Gilroy',
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 45,
+                              const SizedBox(height: 28),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(5, 0, 65, 0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      "$humidity %",
+                                      style: TextStyle(
+                                        color: beeBrown,
+                                        fontFamily: 'Gilroy',
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 45,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 30),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(100, 0, 5, 0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                "$sound",
-                                style: TextStyle(
-                                  color: beeBrown,
-                                  fontFamily: 'Gilroy',
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 45,
+                              SizedBox(height: 30),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(100, 0, 5, 0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "$sound",
+                                      style: TextStyle(
+                                        color: beeBrown,
+                                        fontFamily: 'Gilroy',
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 45,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 35),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(5, 0, 75, 0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: const [
-                              Text(
-                                "400",
-                                style: TextStyle(
-                                  color: beeBrown,
-                                  fontFamily: 'Gilroy',
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 45,
+                              SizedBox(height: 35),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(5, 0, 75, 0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: const [
+                                    Text(
+                                      "400",
+                                      style: TextStyle(
+                                        color: beeBrown,
+                                        fontFamily: 'Gilroy',
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 45,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
+                              // Container(
+                              //   width: 50,
+                              //   margin: EdgeInsets.fromLTRB(80, 0, 0, 0),
+                              //   child: FloatingActionButton(
+                              //     onPressed: () {},
+                              //     backgroundColor: darkOrange,
+                              //     child: Text("Reset"),
+                              //   ),
+                              // )
                             ],
-                          ),
-                        ),
-                        // Container(
-                        //   width: 50,
-                        //   margin: EdgeInsets.fromLTRB(80, 0, 0, 0),
-                        //   child: FloatingActionButton(
-                        //     onPressed: () {},
-                        //     backgroundColor: darkOrange,
-                        //     child: Text("Reset"),
-                        //   ),
-                        // )
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      )),
-    );
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 
   @override
