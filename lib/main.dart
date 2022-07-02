44507ae4c372e415c7d4793b031b733abe63cabf
mohamed-achich <mohamed.achich27@gmail.com>
2022-07-02 16:41:09 +0100

    latest change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'dart:async';
 import 'package:bee/screens/form_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(
     home: MainPage(),
     debugShowCheckedModeBanner: false,
@@ -16,9 +22,7 @@ class MainPage extends StatefulWidget {
   // This widget is the root of your application.
 }
 
-
 class _MainPageState extends State<MainPage> {
-
   PageController pageController = PageController();
   int currentIndex = 0;
 
@@ -47,7 +51,7 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return const AddPage();
   }
+  
 }
